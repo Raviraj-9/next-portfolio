822c6df86e741f8557ba1e54e36804f7f4220b41
Mallanagouda Patil <162907915+Raviraj-9@users.noreply.github.com>
2025-04-30 14:11:42 +0530

    Update hero.tsx

--- components/hero.tsx
+++ components/hero.tsx
@@ -72,7 +72,7 @@ export default function Hero() {
         >
           <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-emerald-600">
             <img
-              src="/pfp.jpg?height=320&width=320"
+              src="/profile.jpg?height=320&width=320"
               alt="Mallangouda Patil"
               className="object-cover w-full h-full"
             />
